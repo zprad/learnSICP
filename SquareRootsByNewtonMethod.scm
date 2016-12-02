543f412eb90a76f4cf078f9b7d462811a63c1217
zengjl <zengcl@gtintel.cn>
2016-12-02 17:31:23 +0800

    symbolic Differentiation

--- SquareRootsByNewtonMethod.scm
+++ SquareRootsByNewtonMethod.scm
@@ -1,1 +1,12 @@
-(define (sqrt x))
+(define (sqrt x)
+	(define (sqrt-iter guess)
+		(if (good-enough? guess)
+			guess
+			(sqrt-iter (improve guess) x)))
+	(define (improve guess)
+		(average guess (/ x guess)))
+	(define (average x y)
+		(/ (+ x y) 2))
+	(define (good-enough? guess)
+		(< (abs (- (square guess) x)) 0.001))
+	(sqrt-iter 1.0))
